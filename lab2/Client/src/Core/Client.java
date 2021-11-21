6ea1ed8a9b2984e2f8d3ec17ef9f13aca783d3e2
Andrew <andrebobrovs@mail.ru>
2021-11-21 14:12:25 +0700

    move project to another directory and renamed it

---
+++ lab2/Client/src/Core/Client.java
@@ -0,0 +1,18 @@
+package Core;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+    private Socket clientSocket;
+    private InputStream socketInputStream;
+    private OutputStream socketOutputStream;
+
+    String filePath;
+
+    // main execute method
+    public void execute() {
+
+    }
+}
